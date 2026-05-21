7b2b810e955f1078e6f16c8107db9f946a81cf11
Christian <cts.colewan@gmail.com>
2026-05-21 23:12:27 +0800

    Add salidumay.com to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,7 @@ return [
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:3000',
+        'https://salidumay.com',
         env('FRONTEND_URL', ''),
     ],
 
